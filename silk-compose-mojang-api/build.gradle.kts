7b61013e47dfeafc72e10ec6eb8efe75aac3ef78
Jakob K <dev@jakobk.de>
2022-06-05 22:28:55 +0200

    Add Compose Mojang API module

---
+++ silk-compose-mojang-api/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+}
+
+description = "FabrikMC Compose Mojang API extracts Minecraft assets from the client jar"
+
+dependencies {
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+}
