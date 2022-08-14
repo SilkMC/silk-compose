cb13f7835db9fdc62cbd5f84d67ea55cf8c11f4a
Jakob K <dev@jakobk.de>
2022-08-15 01:45:59 +0200

    Add rotating wheel test

--- silk-compose-testmod/src/main/kotlin/net/silkmc/silk/test/compose/commands/ComposeCommand.kt
+++ silk-compose-testmod/src/main/kotlin/net/silkmc/silk/test/compose/commands/ComposeCommand.kt
@@ -1,11 +1,16 @@
 package net.silkmc.silk.test.compose.commands
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import net.silkmc.silk.commands.command
 import net.silkmc.silk.compose.displayComposable
 import net.silkmc.silk.compose.ui.McWindowHeader
 import net.silkmc.silk.test.compose.game.FallingBallsGameComposable
 import net.silkmc.silk.test.compose.guis.GeneralTestComposable
+import net.silkmc.silk.test.compose.guis.RotatingWheel
 import net.silkmc.silk.test.compose.guis.ScrollTestComposable
 
 val composeCommand = command("compose") {
@@ -28,6 +33,15 @@ val composeCommand = command("compose") {
     literal("scroll") runs {
         source.playerOrException.displayComposable(5, 4) {
             ScrollTestComposable()
+        }
+    }
+
+    literal("wheel") runs {
+        source.playerOrException.displayComposable(
+            4, 4,
+            backgroundColor = Color.White.copy(0f)
+        ) {
+            RotatingWheel()
         }
     }
 }
---
+++ silk-compose-testmod/src/main/kotlin/net/silkmc/silk/test/compose/guis/RotatingWheel.kt
@@ -0,0 +1,105 @@
+package net.silkmc.silk.test.compose.guis
+
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.loadImageBitmap
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import net.silkmc.silk.core.logging.logInfo
+
+private enum class WheelState {
+    STATIC, ROLLING, STOPPING;
+}
+
+@Composable
+fun RotatingWheel() {
+    val rotatingWheel = rememberBitmapResource("/images/rotating_wheel.png")
+    val staticWheel = rememberBitmapResource("/images/static_wheel.png")
+
+    if (rotatingWheel != null && staticWheel != null) {
+        var wheelState by remember { mutableStateOf(WheelState.STATIC) }
+
+        val rotationAnimatable = remember { Animatable(0f) }
+
+        val rotation = when (wheelState) {
+            WheelState.STATIC -> rotationAnimatable.value
+            WheelState.ROLLING -> {
+                val transition = rememberInfiniteTransition()
+                transition.animateFloat(
+                    initialValue = rotationAnimatable.value, targetValue = rotationAnimatable.value + 360f,
+                    animationSpec = infiniteRepeatable(
+                        animation = tween(700, easing = LinearEasing)
+                    )
+                ).value
+            }
+            WheelState.STOPPING -> {
+                LaunchedEffect(Unit) {
+                    rotationAnimatable.animateTo(
+                        targetValue = rotationAnimatable.value + 360,
+                        animationSpec = tween(700, easing = EaseOut)
+                    )
+                    wheelState = WheelState.STATIC
+                }
+                rotationAnimatable.value
+            }
+        }
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Box {
+                when (wheelState) {
+                    WheelState.STATIC -> {
+                        Button(onClick = {
+                            wheelState = WheelState.ROLLING
+                        }) {
+                            Text("Start rolling")
+                        }
+                    }
+                    WheelState.ROLLING -> {
+                        val stopScope = rememberCoroutineScope()
+                        Button(onClick = {
+                            stopScope.launch {
+                                rotationAnimatable.snapTo(rotation)
+                                wheelState = WheelState.STOPPING
+                            }
+                        }) {
+                            Text("stop wheel")
+                        }
+                    }
+                    else -> {
+                        Button(onClick = {}) {
+                            Text("currently stopping")
+                        }
+                    }
+                }
+            }
+
+            Box {
+                Image(
+                    rotatingWheel, "rotating wheel",
+                    modifier = Modifier.rotate(rotation % 360)
+                )
+                Image(staticWheel, "static wheel")
+            }
+        }
+    }
+}
+
+@Composable
+private fun rememberBitmapResource(path: String): ImageBitmap? {
+    return produceState<ImageBitmap?>(null) {
+        value = withContext(Dispatchers.IO) {
+            logInfo("Loading $path resource as image bitmap")
+            loadImageBitmap(Unit::class.java.getResourceAsStream(path)!!.buffered())
+        }
+    }.value
+}
