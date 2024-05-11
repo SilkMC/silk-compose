50f8f418d74c71608330ad188505cbf206b3c730
Jakob K <dev@jakobk.de>
2024-05-11 05:17:17 +0200

    Increase McIcon size in scroll test composable

--- silk-compose-testmod/src/main/kotlin/net/silkmc/silk/test/compose/guis/ScrollTestComposable.kt
+++ silk-compose-testmod/src/main/kotlin/net/silkmc/silk/test/compose/guis/ScrollTestComposable.kt
@@ -2,11 +2,13 @@ package net.silkmc.silk.test.compose.guis
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import net.silkmc.silk.compose.icons.McIcon
@@ -23,8 +25,8 @@ fun ScrollTestComposable() {
 
     LazyColumn {
         items(icons) { icon ->
-            Row {
-                McImage(icon)
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                McImage(icon, modifier = Modifier.size(50.dp))
                 Spacer(Modifier.width(10.dp))
                 Text(icon)
             }
